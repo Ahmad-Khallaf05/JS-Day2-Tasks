ecdec01c6c655da8d46488b6448559c2909251f9
Ahmad-Khallaf05 <ahmadmajedkhallaf470@gmail.com>
2024-06-05 09:03:20 +0300

    JS-Day2

---
+++ JS.js
@@ -0,0 +1,112 @@
+//-------------task-1----------------
+let x1 = 1 ;
+for (x1;x1<=10;x1+=1) {
+    console.log(x1);
+};
+
+console.log("*********2***********");
+
+//-------------task-2----------------
+let x2 = 0 ;
+while (x2<10) {
+    x2=x2+1 ;
+    console.log(x2);
+};
+
+console.log("*********3***********");
+
+//-------------task-3----------------
+let x3 = 0 ;
+for (x3;x3<=10;x3+=1) {
+    if (x3%2==0){
+        console.log(x3);
+    }
+};
+
+console.log("*********4***********");
+
+//-------------task-4----------------
+let x4 = 1 ;
+var y4 = 0 ;
+for (x4;x4<=10;x4+=1) {
+    var y4=y4+x4;
+};
+console.log(y4);
+
+console.log("*********5***********");
+
+//-------------task-5----------------
+let x5 = 0 ;
+for (x5;x5<=10;x5+=1) {
+    if (x5%2==1){
+        console.log(x5);
+    }
+};
+
+console.log("*********6***********");
+
+//-------------task-6----------------
+let x6 = 19 ;
+if (x6>18) {
+    console.log("You are an adult");
+};
+
+console.log("*********7***********");
+
+//-------------task-7----------------
+let x7 = 45 ;
+if (x7<=50) {
+    console.log("You failed the test");
+};
+
+console.log("*********8***********");
+
+//-------------task-8----------------
+let name8 = "john";
+if (name8 == "john") {
+    console.log("hello, john");
+};
+
+console.log("*********9***********");
+
+//-------------task-9----------------
+let day9 = "Monday" ;
+if (day9 != "Friday" && "Saturday") {
+    console.log("It's a weekday");
+};
+
+console.log("*********10***********");
+
+//-------------task-10----------------
+ let num10 = 4 ;
+ if (num10 % 2 == 0) {
+    console.log("The number is even");
+ };
+
+ console.log("*********11***********");
+
+//-------------task-11----------------
+let x11 = 5 ;
+if (x11>0) {
+    console.log("x is a positive number");
+};
+
+console.log("*********12***********");
+
+//-------------task-12----------------
+let y12 = -5 ; 
+if (y12<0){
+    console.log("y is a negative number");
+};
+
+console.log("*********13***********");
+
+//-------------task-13----------------
+let gba13 = 3.5 ;
+if (gba13>=3){
+    console.log("Congratulations, you have a good GPA!");
+};
+
+console.log("*********14***********");
+
+//-------------task-14----------------
